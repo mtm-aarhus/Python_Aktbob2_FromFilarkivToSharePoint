b025f7c14c6a54a263194e23b2a79bbea4bf7433
Laura-Salmonsen <balas@aarhus.dk>
2025-09-18 14:06:26 +0200

    sandbox as process

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,31 +1,26 @@
-"""This module contains the main process of the robot."""
-
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 from OpenOrchestrator.database.queues import QueueElement
-#Packages
-
 import requests
 from requests_ntlm import HttpNtlmAuth
 import json
-
-#Functions or scripts
 from GetKMDAcessToken import GetKMDToken
 from GetFilarkivToken import GetFilarkivToken
-import GetDocumentList
-import GetDocumentsForAktliste
-import GenerateAndUploadAktliste
-import DownloadFilesFromFilarkivAndUploadToSharePoint
-import SendShareLinkToDeskpro
+from GetDocumentList import get_document_list
+from GetDocumentsForAktliste import GetDocumentsForAktliste
+from DownloadFilesFromFilarkivAndUploadToSharePoint import DownloadFilesFromFilarkivAndUploadToSharePoint
+from GenerateAndUploadAktliste import GenerateAndUploadAktliste
+from SendShareLinkToDeskpro import SendShareLinkToDeskpro
 from SendSMTPMail import send_email # Import the function and dataclass
+import os
+import sys
 
 # pylint: disable-next=unused-argument
 def process(orchestrator_connection: OrchestratorConnection, queue_element: QueueElement | None = None) -> None:
-    """Do the primary process of the robot."""
-    orchestrator_connection.log_trace("Running process.")
 
+    """Do the primary process of the robot."""
     certification = orchestrator_connection.get_credential("SharePointCert")
     api = orchestrator_connection.get_credential("SharePointAPI")
-    
+
     GraphAppIDAndTenant = orchestrator_connection.get_credential("GraphAppIDAndTenant")
     SharePointAppID = GraphAppIDAndTenant.username
     SharePointTenant = GraphAppIDAndTenant.password
@@ -63,7 +58,6 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
     # ---- Initialize Go-Session ----
     go_session = GO_Session(GoUsername, GoPassword)
 
-       #---- Henter kø-elementer ----
     queue = json.loads(queue_element.data)
     Sagsnummer = queue.get("Sagsnummer")
     MailModtager = queue.get("MailModtager")
@@ -76,40 +70,14 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
     NovaSag = queue.get("NovaSag")
     FilarkivCaseID = queue.get("FilarkivCaseID")
 
-
-    # ---- Run "GetDokumentlist" ----sort
-    Arguments = {
-        "in_RobotUserName": RobotUserName,
-        "in_RobotPassword": RobotPassword,
-        "in_Sagsnummer": Sagsnummer,
-        "in_SharePointUrl": SharePointURL,
-        "in_Overmappe": Overmappe,
-        "in_Undermappe": Undermappe,
-        "in_GeoSag": GeoSag, 
-        "in_NovaSag": NovaSag,
-        "GoUsername": GoUsername,
-        "GoPassword":  GoPassword,
-        "KMD_access_token": KMD_access_token,
-        "KMDNovaURL": KMDNovaURL,
-        "in_MailModtager": MailModtager,
-        "tenant": tenant,
-        "client_id": client_id,
-        "thumbprint": thumbprint,
-        "cert_path": cert_path
-    }
-
-
     # ---- Run "GetDocumentList" ----
-    GetDocumentList_Output_arguments = GetDocumentList.invoke(Arguments, go_session)
-    Sagstitel = GetDocumentList_Output_arguments.get("sagstitel")
-    dt_DocumentList = GetDocumentList_Output_arguments.get("dt_DocumentList")
-    DokumentlisteDatoString = GetDocumentList_Output_arguments.get("out_DokumentlisteDatoString")
+    Sagstitel, dt_DocumentList,  DokumentlisteDatoString = get_document_list(Sagsnummer, GeoSag, NovaSag, KMD_access_token, KMDNovaURL, SharePointURL, Overmappe, Undermappe, MailModtager, tenant, client_id, thumbprint, cert_path, go_session)
+    
+    if any(x is None for x in [Sagstitel, dt_DocumentList, DokumentlisteDatoString]):
+        orchestrator_connection.log_info('None returned from doclist. Maybe file transfer is tried before doclist is made.')
+        sys.exit(0)
 
     if dt_DocumentList.empty:
-        print("Number of rows:",len(dt_DocumentList))
-            ###---- Send mail til sagsansvarlig ----####
-
-        # Define email details
         sender = "aktbob@aarhus.dk" 
         subject = f"{Sagsnummer} er en tom sag"
         body = f"""Sagen: {Sagsnummer} er en tom sag. Vær opmærksom på, at processen ikke kan behandle tomme sager.<br><br>
@@ -119,7 +87,6 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
         smtp_server = "smtp.adm.aarhuskommune.dk"   
         smtp_port = 25               
 
-        # Call the send_email function
         send_email(
             receiver=MailModtager,
             sender=sender,
@@ -129,116 +96,11 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
             smtp_port=smtp_port,
             html_body=True
         )
-        raise ValueError("Dokumentlisten inderholder ikke nogen data - Processen fejler")
-    else:
-        print("Number of rows:",len(dt_DocumentList))
+        orchestrator_connection.log_info('Dokumentlisten er tom. Processen afsluttes')
+        sys.exit(0)
 
-    # ---- Run "GetDocumentsForAktliste" ----
-    Arguments_GetDocumentsForAktliste = {
-        "in_dt_Documentlist": dt_DocumentList,
-        "in_CloudConvertAPI": CloudConvertAPI,
-        "in_MailModtager": MailModtager,
-        "in_UdviklerMail": UdviklerMailAktbob,
-        "in_RobotUserName": RobotUserName,
-        "in_RobotPassword": RobotPassword,
-        "in_SharePointAppID": SharePointAppID,
-        "in_SharePointTenant":SharePointTenant,
-        "in_SharePointUrl": SharePointURL,
-        "in_Overmappe": Overmappe,
-        "in_Undermappe": Undermappe,
-        "in_Sagsnummer": Sagsnummer,
-        "in_GeoSag": GeoSag,
-        "in_NovaSag": NovaSag,
-        "in_NovaToken": KMD_access_token,
-        "in_KMDNovaURL": KMDNovaURL,
-        "in_GoUsername": GoUsername,
-        "in_GoPassword":  GoPassword,
-        "tenant": tenant,
-        "client_id": client_id,
-        "thumbprint": thumbprint,
-        "cert_path": cert_path
-    }
-
-    GetDocumentsForAktliste_Output_arguments = GetDocumentsForAktliste.invoke_GetDocumentsForAktliste(Arguments_GetDocumentsForAktliste)
-    dt_AktIndex = GetDocumentsForAktliste_Output_arguments.get("out_dt_AktIndex")
-
-
-
-    # ---- run DownloadFilesFromFilarkivAndUploadToSharePoint ----
-
-    Arguments_DownloadFilesFromFilarkivAndUploadToSharePoint = {
-    "in_dt_AktIndex": dt_AktIndex,
-    "in_FilarkivURL": FilarkivURL,
-    "in_Filarkiv_access_token": Filarkiv_access_token,
-    "in_FilarkivCaseID": FilarkivCaseID,
-    "in_SharePointAppID": SharePointAppID,
-    "in_SharePointTenant": SharePointTenant,
-    "in_SharePointURL": SharePointURL,
-    "in_Overmappe": Overmappe,
-    "in_Undermappe": Undermappe,
-    "in_RobotUserName": RobotUserName,
-    "in_RobotPassword": RobotPassword,
-    "in_MailModtager": MailModtager,
-    "in_Sagsnummer": Sagsnummer,
-    "tenant": tenant,
-    "client_id": client_id,
-    "thumbprint": thumbprint,
-    "cert_path": cert_path
-    }
-
-    DownloadFilesFromFilarkivAndUploadToSharePoint_Output_arguments = DownloadFilesFromFilarkivAndUploadToSharePoint.invoke_DownloadFilesFromFilarkivAndUploadToSharePoint(Arguments_DownloadFilesFromFilarkivAndUploadToSharePoint)
-    Test = DownloadFilesFromFilarkivAndUploadToSharePoint_Output_arguments.get("out_Text")
-    dt_AktIndex = DownloadFilesFromFilarkivAndUploadToSharePoint_Output_arguments.get("out_dt_AktIndex")
-    orchestrator_connection.log_trace(Test) 
-    # ---- Run "Generate&UploadAktlistPDF" ----
-
-    Arguments_GenerateAndUploadAktliste = {
-    "in_dt_AktIndex": dt_AktIndex,
-    "in_Sagsnummer": Sagsnummer,
-    "in_DokumentlisteDatoString":DokumentlisteDatoString, 
-    "in_RobotUserName": RobotUserName,
-    "in_RobotPassword": RobotPassword,
-    "in_SagsTitel": Sagstitel,
-    "in_SharePointAppID": SharePointAppID,
-    "in_SharePointTenant": SharePointTenant,
-    "in_Overmappe": Overmappe,
-    "in_Undermappe": Undermappe,
-    "in_SharePointURL": SharePointURL,
-    "in_GoUsername":GoUsername,
-    "in_GoPassword": GoPassword,
-    "tenant": tenant,
-    "client_id": client_id,
-    "thumbprint": thumbprint,
-    "cert_path": cert_path
-    }
-
-    GenerateAndUploadAktliste_Output_arguments = GenerateAndUploadAktliste.invoke_GenerateAndUploadAktliste(Arguments_GenerateAndUploadAktliste)
-    Test = GenerateAndUploadAktliste_Output_arguments.get("out_Text")
-    orchestrator_connection.log_trace(Test)
-
-    ##---- run SendShareLinkToDeskpro ----##
-
-    Arguments_SendShareLinkToDeskpro = {
-
-    "in_SharePointAppID": SharePointAppID,
-    "in_SharePointTenant": SharePointTenant,
-    "in_SharePointURL": SharePointURL,
-    "in_Overmappe": Overmappe,
-    "in_Undermappe": Undermappe,
-    "in_RobotUserName": RobotUserName,
-    "in_RobotPassword": RobotPassword,
-    "in_PodioID": PodioID,
-    "in_AktbobAPIKey": AktbobAPIKey,
-    "in_DeskProID": DeskProID,
-    "in_DeskProTitel": DeskProTitel,
-    "in_MailModtager": MailModtager,
-    "in_Sagsnummer": Sagsnummer,
-    "tenant": tenant,
-    "client_id": client_id,
-    "thumbprint": thumbprint,
-    "cert_path": cert_path
-    }
-     
-    SendShareLinkToDeskpro_Output_arguments = SendShareLinkToDeskpro.invoke_SendShareLinkToDeskpro(Arguments_SendShareLinkToDeskpro,orchestrator_connection)
-    Test = SendShareLinkToDeskpro_Output_arguments.get("out_Text")
-    orchestrator_connection.log_trace(Test)
+    dt_AktIndex = GetDocumentsForAktliste(dt_DocumentList = dt_DocumentList, Overmappe = Overmappe, Undermappe = Undermappe, Sagsnummer = Sagsnummer, GeoSag = GeoSag, KMDNovaURL = KMDNovaURL, KMD_access_token = KMD_access_token, go_session = go_session)
+    dt_AktIndex = DownloadFilesFromFilarkivAndUploadToSharePoint(FilarkivURL, Filarkiv_access_token, dt_AktIndex, FilarkivCaseID, SharePointURL, Overmappe, Undermappe, MailModtager, Sagsnummer, tenant, client_id, thumbprint, cert_path, orchestrator_connection )
+    GenerateAndUploadAktliste(dt_AktIndex, Sagsnummer, DokumentlisteDatoString,SharePointURL, Overmappe, Undermappe, tenant, client_id, thumbprint, cert_path )
+    SendShareLinkToDeskpro(SharePointURL, Overmappe, PodioID, AktbobAPIKey, DeskProID, MailModtager, Sagsnummer, tenant, client_id, thumbprint, cert_path, orchestrator_connection)
+    
