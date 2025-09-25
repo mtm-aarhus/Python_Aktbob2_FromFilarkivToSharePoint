2ed2bbd23be7c4d129d3a060873ca6482a130ea2
Laura-Salmonsen <balas@aarhus.dk>
2025-09-25 15:45:10 +0200

    Update DownloadFilesFromFilarkivAndUploadToSharePoint.py

--- DownloadFilesFromFilarkivAndUploadToSharePoint.py
+++ DownloadFilesFromFilarkivAndUploadToSharePoint.py
@@ -80,8 +80,6 @@ def download_files(FilarkivURL, FilarkivCaseID, Filarkiv_access_token, Sagsnumme
                     file_path = os.path.join("C:\\Users", os.getlogin(), "Downloads", file_name)
                     print(f'Getting {file_name}')
                     response = requests.get(download_url, headers=headers)
-                    if response.status_code == 404:
-                        continue
                     response.raise_for_status()
 
                     with open(file_path, "wb") as f:
